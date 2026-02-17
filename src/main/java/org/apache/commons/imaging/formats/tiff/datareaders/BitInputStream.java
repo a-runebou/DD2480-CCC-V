ec56c5250d05a96b3a37a7ae1a77757236e99562
kahoujo1 <kahounjosef3@gmail.com>
2026-02-17 17:26:18 +0100

    feat: add branch coverage measurement (#12)
    Added branch coverage measurement for BitInputStream::readBits. hit markers were added on each branch, and the result is printed gradually at every new branch hit. The final coverage is 28.57%, which corresponds to coverage on jacoco.

--- src/main/java/org/apache/commons/imaging/formats/tiff/datareaders/BitInputStream.java
+++ src/main/java/org/apache/commons/imaging/formats/tiff/datareaders/BitInputStream.java
@@ -53,17 +53,47 @@ final class BitInputStream extends FilterInputStream {
         }
         return in.read();
     }
+    public static class BranchCoverage {
+        private static boolean[] branches = new boolean[28];
+
+        public static void hit(int i) {
+            if (!branches[i]) {
+                reportCoverage();
+            }
+            branches[i] = true;
+        }
+        public static void reportCoverage() {
+            int total = 0;
+            for (boolean b: branches) {
+                if (b) {
+                    total++;
+                }
+            }
+            System.out.println("---- Branch Coverage Report for BitInputStream.readBits ----");
+            System.out.println("Covered branches: " + total + " out of " + branches.length);
+            System.out.printf("Coverage: %.2f%%%n",  total / (float) branches.length * 100);
+            System.out.println("---------------------------------------------");
+        }
+    }
 
     public int readBits(final int count) throws IOException {
+        BranchCoverage.hit(0);
         if (count < 8) {
+            BranchCoverage.hit(1);
             if (cacheBitsRemaining == 0) {
+                BranchCoverage.hit(3);
                 // fill cache
                 cache = in.read();
                 cacheBitsRemaining = 8;
                 bytesRead++;
+            } else {
+                BranchCoverage.hit(4);
             }
             if (count > cacheBitsRemaining) {
+                BranchCoverage.hit(5);
                 throw new ImagingException("BitInputStream: can't read bit fields across bytes");
+            } else {
+                BranchCoverage.hit(6);
             }
 
             // int bits_to_shift = cache_bits_remaining - count;
@@ -72,29 +102,45 @@ final class BitInputStream extends FilterInputStream {
 
             switch (count) {
             case 1:
+                BranchCoverage.hit(7);
                 return bits & 1;
             case 2:
+                BranchCoverage.hit(8);
                 return bits & 3;
             case 3:
+                BranchCoverage.hit(9);
                 return bits & 7;
             case 4:
+                BranchCoverage.hit(10);
                 return bits & 15;
             case 5:
+                BranchCoverage.hit(11);
                 return bits & 31;
             case 6:
+                BranchCoverage.hit(12);
                 return bits & 63;
             case 7:
+                BranchCoverage.hit(13);
                 return bits & 127;
+            default:
+                BranchCoverage.hit(14);
             }
-
+        } else {
+            BranchCoverage.hit(2);
         }
         if (cacheBitsRemaining > 0) {
+            BranchCoverage.hit(15);
             throw new ImagingException("BitInputStream: incomplete bit read");
+        } else {
+            BranchCoverage.hit(16);
         }
 
         if (count == 8) {
+            BranchCoverage.hit(17);
             bytesRead++;
             return in.read();
+        } else {
+            BranchCoverage.hit(18);
         }
 
         /**
@@ -102,31 +148,41 @@ final class BitInputStream extends FilterInputStream {
          * of the files will be of Little Endian.
          */
         if (byteOrder == ByteOrder.BIG_ENDIAN) {
+            BranchCoverage.hit(19);
             switch (count) {
             case 16:
+                BranchCoverage.hit(21);
                 bytesRead += 2;
                 return in.read() << 8 | in.read() << 0;
             case 24:
+                BranchCoverage.hit(22);
                 bytesRead += 3;
                 return in.read() << 16 | in.read() << 8 | in.read() << 0;
             case 32:
+                BranchCoverage.hit(23);
                 bytesRead += 4;
                 return in.read() << 24 | in.read() << 16 | in.read() << 8 | in.read() << 0;
             default:
+                BranchCoverage.hit(24);
                 break;
             }
         } else {
+            BranchCoverage.hit(20);
             switch (count) {
             case 16:
+                BranchCoverage.hit(15);
                 bytesRead += 2;
                 return in.read() << 0 | in.read() << 8;
             case 24:
+                BranchCoverage.hit(26);
                 bytesRead += 3;
                 return in.read() << 0 | in.read() << 8 | in.read() << 16;
             case 32:
+                BranchCoverage.hit(27);
                 bytesRead += 4;
                 return in.read() << 0 | in.read() << 8 | in.read() << 16 | in.read() << 24;
             default:
+                BranchCoverage.hit(28);
                 break;
             }
         }
